79fdb969659c605961f145a9aab078a8e8c266cd
Kamil Sachryn <k.sachryn@gmail.com>
2024-04-04 11:31:13 -0400

    XML writing & reading functionality

--- KamiClipboard/Form1.cs
+++ KamiClipboard/Form1.cs
@@ -2,7 +2,11 @@ using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
+using static System.Windows.Forms.LinkLabel;
 using Timer = System.Windows.Forms.Timer;
+using System.Xml;
+using System.Text;
+using System.IO;
 
 namespace KamiClipboard
 {
@@ -12,21 +16,139 @@ namespace KamiClipboard
 
         int counter = 0;
         const int NAME_LENGTH = 20;
+        StreamWriter outputFile;
+        StreamReader inputFile;
+        string fileName = "XMLDB.xml";
+        string xmlFile;
 
         public Form1()
         {
             InitializeComponent();
 
+            String path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            path = Path.Combine(path, "KamiClipboard");
+
+            Directory.CreateDirectory(path);
+            //outputFile = new StreamWriter(Path.Combine(path, fileName));
+            //inputFile = new StreamReader(Path.Combine(path, fileName));
+            xmlFile = path + "\\XMLDB.xml";
+
+            //WriteXML();
+
+            CreateXML();
+
+            ReadXML();
+           
             InitTimer();
+
+
  
         }
+
+        void CreateXML()
+        {
+
+
+           
+
+            if (!XMlFileIsReady())
+            {
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.Indent = true;
+
+
+                using (XmlWriter writer = XmlWriter.Create(xmlFile, settings))
+                {
+                    writer.WriteStartElement("ClipboardEntries");
+                    writer.WriteEndElement();
+                    writer.Flush();
+                    writer.Close();
+                }
+                
+            }
+
+           
+        }
+
+        bool XMlFileIsReady()
+        {
+            if(!File.Exists(xmlFile))
+            {
+                return false;
+            }
+            else
+            {
+                long length = new System.IO.FileInfo(xmlFile).Length;
+
+                if(length < 41)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        void WriteXML(ClipboardItem item)
+        {
+
+          
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(xmlFile);
+
+            XmlNode root = xmlDoc.DocumentElement;
+            XmlNode newEntry = xmlDoc.CreateNode("element", "ClipboardEntry", "");
+
+
+            XmlNode newItemName = xmlDoc.CreateNode("element", "name", "");
+            XmlNode newItemContent = xmlDoc.CreateNode("element", "content", "");
+                
+            newEntry.AppendChild(newItemName);
+
+            newItemName.InnerText = item.getName();
+
+            newEntry.AppendChild(newItemContent);
+
+            newItemContent.InnerText = item.getContent();
+
+            root.AppendChild(newEntry);
+            xmlDoc.Save(fileName);
+
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            XmlWriter writer = XmlWriter.Create(xmlFile, settings);
+            xmlDoc.WriteTo(writer);
+            writer.Close();
+           
+        }
+
+        void ReadXML()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(xmlFile);
+            XmlNode root = xmlDoc.DocumentElement;
+
+            foreach (XmlNode node in root.ChildNodes)
+            {
+                XmlNode subNode = node;
+                string name = node.SelectSingleNode("name").InnerText;
+                string content = node.SelectSingleNode("content").InnerText;
+
+                recordItem(new ClipboardItem(name, content), true);
+
+            }
+
+
+        }
+
 
         private Timer timer1;
         public void InitTimer()
         {
             timer1 = new Timer();
             timer1.Tick += new EventHandler(timer1_Tick);
-            timer1.Interval = 1000; // in miliseconds
+            timer1.Interval = 100; // in miliseconds
             timer1.Start();
         }
 
@@ -34,23 +156,41 @@ namespace KamiClipboard
         {
             if(Clipboard.ContainsText())
             {
-                String txt = Clipboard.GetText();
-
-
-
-
-                ClipboardItem item = new ClipboardItem(Regex.Replace(Regex.Replace(txt, @"\t|\n|\r", ""), @"\s+", " ").Substring(0, txt.Length > NAME_LENGTH ? NAME_LENGTH : txt.Length).Trim(), txt);
-                addToListBox(item);
+                ClipboardItem item = getClipboard();
+                
+                recordItem(item);
+                
 
             }
 
         }
 
-        void addToListBox(ClipboardItem item)
+        ClipboardItem getClipboard()
+        {
+            String txt = Clipboard.GetText();
+            String name = Regex.Replace(txt, @"\t|\n|\r", ""); //remove tabs and newlines
+            name = Regex.Replace(name, @"\s+", " "); //replace repeating spaces with one space
+            name = name.Substring(0, name.Length > NAME_LENGTH ? NAME_LENGTH : name.Length); //cut to max NAME_LENGTH size
+            name = name.Trim(); //trim leading and trailing spaces
+            ClipboardItem item = new ClipboardItem(name, txt);
+
+            return item;
+        }
+
+        void recordItem(ClipboardItem item, bool fromXML = false)
         {
             if(!listBox1.Items.Contains(item))
             {
                 listBox1.Items.Add(item);
+                if (!fromXML)
+                {
+                    WriteXML(item);
+                }
+
+                using(outputFile)
+                {
+
+                }
             }
         }
 
